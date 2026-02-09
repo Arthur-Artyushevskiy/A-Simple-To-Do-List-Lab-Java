122c38e40d06f00c4ed9d1fbb42d39ff328f5a5f
Arthur-Artyushevskiy <artyushevskiy@outlook.com>
2026-02-08 18:27:47 -0600

    CSC214 Lab

---
+++ app/src/main/java/org/example/TodoList.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class TodoList {
+ private Hashtable<String, Boolean> list;
+
+ TodoList(){
+   list = new Hashtable<>();
+ }
+
+ void add(String name){
+     list.put(name, false);
+ }
+
+ void complete(String name){
+     if(!list.isEmpty()){
+         list.replace(name, true);
+     }
+     else{
+         System.out.println("Error: Your List is Empty. Can't set a non-existing task as complete!");
+     }
+ }
+
+ ArrayList<String> all(){
+     System.out.println("List of Tasks:");
+     ArrayList<String> allList = new ArrayList<>();
+     for(Map.Entry<String, Boolean> entry : list.entrySet()){
+        String key = entry.getKey();
+         allList.add(key);
+        System.out.println(key);
+
+     }
+  return allList;
+ }
+
+ArrayList<String> complete(){
+    System.out.println("List of Completed Tasks:");
+    ArrayList<String> completeList = new ArrayList<>();
+    for(Map.Entry<String, Boolean> entry : list.entrySet()){
+        String key = entry.getKey();
+        Boolean isComplete = entry.getValue();
+
+        if(isComplete){
+            completeList.add(key);
+            System.out.println(key);
+        }
+    }
+    return completeList;
+}
+
+ ArrayList<String> incomplete(){
+     System.out.println("List of Incompleted Tasks:");
+     ArrayList<String> incompleteList = new ArrayList<>();
+     for(Map.Entry<String, Boolean> entry : list.entrySet()){
+         String key = entry.getKey();
+         Boolean isComplete = entry.getValue();
+
+         if(!isComplete){
+             incompleteList.add(key);
+             System.out.println(key);
+         }
+     }
+    return incompleteList;
+ }
+
+ void clear(){
+     list.clear();
+ }
+
+}
--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -1,5 +1,58 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class AppTest {
-  // Put your tests here!
+  @Test
+    void testAddTask(){
+      var list = new TodoList();
+      list.add("Buy groceries");
+      ArrayList<String> all = list.all();
+      assertEquals(1, all.size());
+      assertTrue(all.contains("Buy groceries"));
+  }
+
+  @Test
+    void testIncompleteFilter(){
+      var list = new TodoList();
+      list.add("Task 1");
+      list.add("Task 2");
+      list.complete("Task 1");
+
+      ArrayList<String> incomplete = list.incomplete();
+
+      assertEquals(1, incomplete.size());
+      assertTrue(incomplete.contains("Task 2"));
+      assertFalse(incomplete.contains("Task 1"));
+  }
+
+  @Test
+    void completeFilter(){
+      var list = new TodoList();
+      list.add("Workout");
+      list.complete("Workout");
+
+      ArrayList<String> completed = list.complete();
+      assertEquals(1, completed.size());
+      assertTrue(completed.contains("Workout"));
+  }
+
+  @Test
+    void testClearList(){
+      var list = new TodoList();
+      list.add("Workout");
+      list.complete("Workout");
+      list.clear();
+      ArrayList<String> all = list.all();
+      assertEquals(0, all.size());
+  }
+
+  @Test
+    void testCompleteNonExistingTask(){
+      var list = new TodoList();
+      list.complete("Ghost Task");
+  }
 }
