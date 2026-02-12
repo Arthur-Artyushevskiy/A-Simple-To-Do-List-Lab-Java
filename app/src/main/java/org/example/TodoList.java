92af072261850d3dcbbe2807a677290f3748a3d0
Arthur-Artyushevskiy <artyushevskiy@outlook.com>
2026-02-12 17:20:15 -0600

    Update TodoList.java

--- app/src/main/java/org/example/TodoList.java
+++ app/src/main/java/org/example/TodoList.java
@@ -13,6 +13,10 @@ public class TodoList {
  }
 
  void add(String name){
+     if(name.isEmpty() || name == null){
+         System.out.println("Your task is empty! No task is added to the list.");
+         return;
+     }
      list.put(name, false);
  }
 
@@ -40,6 +44,12 @@ public class TodoList {
 ArrayList<String> complete(){
     System.out.println("List of Completed Tasks:");
     ArrayList<String> completeList = new ArrayList<>();
+
+    if(list.size() == 0){
+        System.out.println("Your list is empty! Nothing is printed out.");
+        return completeList;
+    }
+
     for(Map.Entry<String, Boolean> entry : list.entrySet()){
         String key = entry.getKey();
         Boolean isComplete = entry.getValue();
